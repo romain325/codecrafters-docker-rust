4ec828c3f15dc0caab91eefe51505b49a7f40538
romain325 <romainolivier42@gmail.com>
2022-04-13 22:33:51 +0200

    maybe

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,13 @@ fn main() {
     
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout).unwrap();
-        println!("{}", std_out)
+        let std_err = std::str::from_utf8(&output.stderr).unwrap();
+        if !std_out.is_empty() {
+            println!("{}", std_out);
+        }
+        if !std_err.is_empty() {
+            println!("{}", std_err);
+        }
     } else {
         std::process::exit(1);
     }
